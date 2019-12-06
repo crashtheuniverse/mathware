d72584974be2c5d8111747834802a8ad3cc7cacb
CrashTheuniversE <crashtheuniverse@gmail.com>
2019-12-06 16:22:16 -0600

    Added v2,v3,v4

--- index.js
+++ index.js
@@ -3,17 +3,22 @@
 const mw = {};
 
 var vec2 = require('./lib/vec2');
+var vec3 = require('./lib/vec3');
+var vec4 = require('./lib/vec4');
 
 function printMsg() {
   console.log("This is a message from the demo package");
 }
 
-function v2() { return new vec2(); }
-
 mw.printMsg = printMsg;
 
 mw.vec2 = vec2;
-mw.v2 = v2; 
+mw.vec3 = vec3; 
+mw.vec4 = vec4;
+
+mw.v2 = function() { return new vec2(); }
+mw.v3 = function() { return new vec3(); }
+mw.v4 = function() { return new vec4(); } 
 
 module.exports = mw; 
 

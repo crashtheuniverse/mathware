0043bd7ec63bcd778acc0b17f9f8d9fd06371c14
CrashTheuniversE <crashtheuniverse@gmail.com>
2019-12-08 12:54:12 -0600

    Updated commodity functions to accept params with defaults

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ mw.vec2 = vec2;
 mw.vec3 = vec3; 
 mw.vec4 = vec4;
 
-mw.v2 = function() { return new vec2(); }
-mw.v3 = function() { return new vec3(); }
-mw.v4 = function() { return new vec4(); } 
+mw.v2 = function(x = 0.0, y = 0.0) { return new vec2(x,y); }
+mw.v3 = function(x = 0.0, y = 0.0, z = 0.0) { return new vec3(x,y,z); }
+mw.v4 = function(x = 0.0, y = 0.0, z = 0.0, w = 1.0) { return new vec4(x,y,z,w); } 
 
 module.exports = mw; 
 
